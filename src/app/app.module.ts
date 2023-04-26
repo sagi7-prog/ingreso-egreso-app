58c015f01f393d637c3fd0b57470b326315dedc6
RafaelHernandez Hernandez <sagisiete7@gmail.com>
2023-04-26 11:08:30 -0600

    fix store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {isDevMode, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
-import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
-import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import {AngularFireAnalyticsModule} from "@angular/fire/compat/analytics";
 import {environment} from "../environments/environment";
+
+// AngularFire
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+// import {AngularFireAnalyticsModule} from "@angular/fire/compat/analytics";
+
+import {AppRoutingModule} from './app-routing.module';
+
+import {ReactiveFormsModule} from "@angular/forms";
+
+import {AppComponent} from './app.component';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {IngresoEgresoComponent} from './ingreso-egreso/ingreso-egreso.component';
+import {EstadisticaComponent} from './ingreso-egreso/estadistica/estadistica.component';
+import {DetalleComponent} from './ingreso-egreso/detalle/detalle.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import {NavbarComponent} from './shared/navbar/navbar.component';
+import {SidebarComponent} from './shared/sidebar/sidebar.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,11 +43,12 @@ import {environment} from "../environments/environment";
     AppRoutingModule,
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAnalyticsModule,
+    // AngularFireAnalyticsModule,
     AngularFirestoreModule,
     AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
